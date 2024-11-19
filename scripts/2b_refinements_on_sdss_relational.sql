30138b889054eb91012339d912b90e191bb92022
Enrico Gallinucci <enrico.gallinucci@unibo.it>
2024-11-19 18:29:42 +0100

    minor updates

--- scripts/2b_refinements_on_sdss_relational.sql
+++ scripts/2b_refinements_on_sdss_relational.sql
@@ -1,3 +1,11 @@
 alter table sdss_relational.frame add column img bytea;
 
 update sdss_relational.frame set img = (select img from db_2023.frame limit 1);
+
+alter table sdss_relational2.platex add column plateidnew double precision;
+
+update sdss_relational2.platex set plateidnew = cast(plateid as double precision);
+
+alter table sdss_relational2.platex drop column plateid;
+
+alter table sdss_relational2.platex rename column plateidnew to plateid;
