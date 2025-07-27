e0f2be53edcefb075ffd2f035f0e4da6af2fa7ef
Alberto <aabello@essi.upc.edu>
2025-07-27 15:17:04 +0200

    Greedy search implemented

--- SchedulingMigration/main.py
+++ SchedulingMigration/main.py
@@ -15,7 +15,45 @@ best = 0
 worse = 999999999
 best_plan = [0]
 worse_plan = [0]
-steps = 0
+
+
+def add_benefit_to_nodes():
+    for current in G.nodes:
+        node = G.nodes[current]
+        if node["kind"] == "Query":
+            node["benefit"] = node["weight"] * (node["time_before"] - node["time_after"])
+
+
+def add_cumulative_duration_to_nodes(visited):
+    ready = [0]
+    while ready != []:
+        current = ready.pop()
+        if current not in visited:
+            cum_dur = G.nodes[current]["duration"]
+            for node in G.predecessors(current):
+                cum_dur += G.nodes[node]["duration"]
+            G.nodes[current]["cumulative_duration"] = cum_dur
+            visited.append(current)
+        for node in G.successors(current):
+            if all([n in visited for n in G.predecessors(node)]):
+                ready.append(node)
+
+def annotate_queries_in_migrations():
+    ready = [99]
+    visited = []
+    while ready != []:
+        current = ready.pop()
+        if G.nodes[current]["kind"] == "Query":
+            G.nodes[current]["impacted_queries"] = [current]
+        else:
+            impacted = []
+            for node in G.successors(current):
+                impacted.extend(G.nodes[node]["impacted_queries"])
+            G.nodes[current]["impacted_queries"] = impacted
+        visited.append(current)
+        for node in G.predecessors(current):
+            if all([n in visited for n in G.successors(node)]):
+                ready.append(node)
 
 
 def load_graph(file_path) -> None:
@@ -27,21 +65,18 @@ def load_graph(file_path) -> None:
         nodes = [tuple(item) for item in graph_elements.get('nodes')]
         edges = [tuple(item) for item in graph_elements.get('edges')]
 
-    add_benefit_to_nodes(nodes)
-
     G.add_nodes_from(nodes)
     G.add_edges_from(edges)
+
+    add_benefit_to_nodes()
+    annotate_queries_in_migrations()
+    # for i in G.nodes:
+    #     print(G.nodes[i])
 
 
 def show_graph():
     nx.draw_shell(G, with_labels=True, font_weight='bold')
     plt.show()
-
-
-def add_benefit_to_nodes(nodes):
-    for node in nodes:
-        if node[1]["kind"] == "Query":
-            node[1]["benefit"] = node[1]["weight"] * (node[1]["time_before"] - node[1]["time_after"])
 
 
 def get_ready_nodes(nodes, visited) -> list[int]:
@@ -117,6 +152,47 @@ def exhaustive_search(plan, ready, plan_tail):
     _ = [plan.pop() for _ in range(len(ready_queries))]
 
 
+def greedy_search(plan, ready, plan_tail):
+    global steps
+    global benefit
+
+    steps += 1
+
+    # Add all queries to the plan at once (no need for backtracking)
+    ready_queries = [i for i in ready if G.nodes[i]["kind"] == "Query" and G.nodes[i]["benefit"] > 0]
+    plan_tail.extend([i for i in ready if G.nodes[i]["kind"] == "Query" and G.nodes[i]["benefit"] <= 0])
+    plan.extend(ready_queries)
+    update_benefit_of_plan(ready_queries, sign=+1)
+    # Take one more migration
+    ready_migrations = [i for i in ready if G.nodes[i]["kind"] == "Migration"]
+    if not ready_migrations:
+        plan.extend(plan_tail)
+        print(f"Worse plan: {plan} -> {benefit:.2f}")
+    else:
+        add_cumulative_duration_to_nodes(plan.copy())
+        heuristics = []
+        pending_benefit = sum([G.nodes[n]["benefit"] for n in G.nodes if n not in plan and G.nodes[n]["kind"] == "Query" and G.nodes[n]["benefit"] > 0])
+        for node in ready_migrations:
+            pending_duration = sum([G.nodes[n]["duration"] for n in G.nodes if n != node and n not in plan and G.nodes[n]["kind"] == "Migration"])
+            h = 0
+            related_benefit = 0
+            for q in G.nodes[node]["impacted_queries"]:
+                h += pending_duration * G.nodes[q]["benefit"] * (G.nodes[node]["duration"] / G.nodes[q]["cumulative_duration"])
+            h -= (pending_benefit - related_benefit) * G.nodes[node]["duration"]
+            heuristics.append(h)
+        current = ready_migrations[heuristics.index(max(heuristics))]
+        plan.append(current)
+        # Increase the benefit of the current migration
+        update_benefit_of_plan([current], sign=+1)
+        # Add new nodes that became ready
+        next_ready = [n for n in ready_migrations if n != current]
+        for j in G.successors(current):
+            if j not in plan and all([n in plan for n in G.predecessors(j)]):
+                next_ready.append(j)
+        # Recursive call
+        greedy_search(plan, next_ready, plan_tail)
+
+
 if __name__ == '__main__':
     if len(sys.argv) < 2:
         print("Error: Input filename required as a parameter")
@@ -124,12 +200,17 @@ if __name__ == '__main__':
 
     load_graph(Path("inputs").joinpath(sys.argv[1]))
 
+    steps = 0
     start = time.time()
     exhaustive_search([0], get_ready_nodes(G.nodes, [0]), [])
     end = time.time()
-
-    print(f"{steps} search steps executed in {(end - start):.2f} seconds")
-
     print(f"Best plan: {best_plan} -> {best:.2f}")
     print(f"Worse plan: {worse_plan} -> {worse:.2f}")
+    print(f"{steps} search steps executed in {(end - start):.2f} seconds")
+
+    steps = 0
+    start = time.time()
+    greedy_search([0], get_ready_nodes(G.nodes, [0]), [])
+    end = time.time()
+    print(f"{steps} search steps executed in {(end - start):.2f} seconds")
 
